8a9d377390f1d7203bfbb762d3db82b4fd6c2c84
Qasim <malikqasim20051@gmail.com>
2026-03-22 04:54:07 +0500

    weather api signature using pydantic

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,7 +1,15 @@
 import fastapi
+from typing import Optional
+from fastapi.params import Depends
+from models.location import Location
+
 
 router = fastapi.APIRouter()
 
-@router.get('/api/weather')
-def weather():
-    return "Some Report"
+
+
+@router.get('/api/weather/{city}')
+def weather(loc: Location = Depends(), 
+            units: Optional[str] = "metric"):
+    
+    return f"{loc.city}, {loc.state}, {loc.country} in {units}"
