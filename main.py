d243b2b7b5de3c2c30a8ce25fccfda77756a1ccd
Qasim <malikqasim20051@gmail.com>
2026-03-22 00:35:34 +0500

    Connecting Files & Routing

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import fastapi
-from starlette.requests import Request
-from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 import uvicorn
+
+from views import home
+from api import weather_api
 
 
 api =fastapi.FastAPI()
-templates = Jinja2Templates(directory="templates")
 
 
-@api.get("/")
-def index(request: Request):
-    return templates.TemplateResponse("index.html",{"request" : request})
+def configure():
+    configure_routing()
+
+
+def configure_routing():
+    api.mount("/static", StaticFiles(directory="static"), name="static")
+    api.include_router(home.router)
+    api.include_router(weather_api.router)      
+
+
 
 
 if __name__ == "__main__":
+    configure()
     uvicorn.run(api, host='127.0.0.1', port=8000) 
+else:
+    configure()
