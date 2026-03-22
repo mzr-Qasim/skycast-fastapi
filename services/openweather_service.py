bdfb67dea60de87637ea0ab97a4c27a965d83ccf
Qasim <malikqasim20051@gmail.com>
2026-03-23 01:32:50 +0500

    calling weather api synchronously

--- services/openweather_service.py
+++ services/openweather_service.py
@@ -1,8 +1,19 @@
 from typing import Optional
+import requests
 
 api_key: Optional [str] = None
 
 def get_report(city: str, state: Optional[str], country : str, units: str) -> dict:
+    if state:
+        q = f'{city}, {state}, {country}'
+    else:
+        q = f'{city}, {country}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}'
 
-    q = f'{city}, {country}'
-    url = f'https://api.openweathermap.org/geo/1.0/direct?q={q}&appid={api_key}?units={units}' 
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+
+    data = resp.json()
+    forcast = data['main']
+    return forcast
